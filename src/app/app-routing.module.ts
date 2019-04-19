de250bc648c5678b0f01075dd8c9869631a882fb
josea2017 <josea3712@gmail.com>
2019-04-18 18:19:06 -0600

    Inicio y consumo de Schedules services2

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'schedule', loadChildren: './schedule/schedule.module#SchedulePageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/tab2/tab2.module.ts
@@ -0,0 +1,29 @@
+//import { IonicModule, NavController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab2Page } from './tab2.page';
+
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: '', component: Tab2Page }]),
+  ],
+  declarations: [Tab2Page]
+})
+export class Tab2PageModule {
+
+  constructor(public router: Router){
+
+  }
+  schedulePage(){
+    this.router.navigateByUrl(['/schedule']);
+  }
+}
